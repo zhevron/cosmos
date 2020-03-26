f14d61c2a9e356f7458430c0a1a1f4430c93be5b
Thomas Lokshall <thomas@lokshall.no>
2020-03-26 08:50:54 +0000

    add utility methods to check error codes

--- error.go
+++ error.go
@@ -28,3 +28,54 @@ type CosmosError struct {
 func (e *CosmosError) Error() string {
 	return fmt.Sprintf("cosmosdb error: code=%d message=%s", e.Code, e.Message)
 }
+
+func IsNoDocumentID(err error) bool {
+	return isErrorCode(err, ErrNoDocumentID)
+}
+
+func IsInvalidKey(err error) bool {
+	return isErrorCode(err, ErrInvalidKey)
+}
+
+func IsTimeout(err error) bool {
+	return isErrorCode(err, ErrTimeout)
+}
+
+func InUnauthorized(err error) bool {
+	return isErrorCode(err, ErrUnauthorized)
+}
+
+func IsForbidden(err error) bool {
+	return isErrorCode(err, ErrForbidden)
+}
+
+func IsBadRequest(err error) bool {
+	return isErrorCode(err, ErrBadRequest)
+}
+
+func IsNotFound(err error) bool {
+	return isErrorCode(err, ErrNotFound)
+}
+
+func IsConflict(err error) bool {
+	return isErrorCode(err, ErrConflict)
+}
+
+func IsConcurrency(err error) bool {
+	return isErrorCode(err, ErrConcurrency)
+}
+
+func IsDocumentTooLarge(err error) bool {
+	return isErrorCode(err, ErrDocumentTooLarge)
+}
+
+func IsInternalServerError(err error) bool {
+	return isErrorCode(err, ErrInternalServerError)
+}
+
+func isErrorCode(err error, code ErrorCode) bool {
+	if cerr, ok := err.(*CosmosError); ok {
+		return cerr.Code == code
+	}
+	return false
+}
