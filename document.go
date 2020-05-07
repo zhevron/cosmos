1032bd45b517be8bc527b798db83bb31a0122382
Thomas Lokshall <thomas@lokshall.no>
2020-05-07 13:17:35 +0000

    extract id field from structs even without json tag

--- document.go
+++ document.go
@@ -2,9 +2,14 @@ package cosmos
 
 import (
 	"reflect"
+	"strings"
 )
 
 func DocumentID(document interface{}) (string, error) {
+	if doc, ok := document.(Document); ok {
+		return doc.ID, nil
+	}
+
 	rv := reflect.ValueOf(document)
 	if rv.Kind() == reflect.Ptr {
 		rv = rv.Elem()
@@ -12,36 +17,59 @@ func DocumentID(document interface{}) (string, error) {
 
 	switch rv.Kind() {
 	case reflect.Map:
-		keys := rv.MapKeys()
-		if len(keys) == 0 {
-			return "", &CosmosError{Code: ErrNoDocumentID, Message: "map has no keys"}
-		}
-
-		if keys[0].Kind() != reflect.String {
-			return "", &CosmosError{Code: ErrNoDocumentID, Message: "map keys are not strings"}
-		}
-
-		for _, k := range keys {
-			if k.String() == "id" {
-				if id, ok := rv.MapIndex(k).Interface().(string); ok && id != "" {
-					return id, nil
-				}
-
-				return "", &CosmosError{Code: ErrNoDocumentID, Message: "could not convert id to string"}
-			}
-		}
+		return extractDocumentIDFromMap(rv)
 
 	case reflect.Struct:
-		rt := rv.Type()
-		numField := rt.NumField()
-		for i := 0; i < numField; i++ {
-			if rt.Field(i).Tag.Get("json") == "id" {
-				if id, ok := rv.Field(i).Interface().(string); ok && id != "" {
-					return id, nil
-				}
+		return extractDocumentIDFromStruct(rv)
+	}
 
-				return "", &CosmosError{Code: ErrNoDocumentID, Message: "could not convert id to string"}
+	return "", &CosmosError{Code: ErrNoDocumentID, Message: "unsupported document type"}
+}
+
+func extractDocumentIDFromMap(document reflect.Value) (string, error) {
+	keys := document.MapKeys()
+	if len(keys) == 0 {
+		return "", &CosmosError{Code: ErrNoDocumentID, Message: "map has no keys"}
+	}
+
+	if keys[0].Kind() != reflect.String {
+		return "", &CosmosError{Code: ErrNoDocumentID, Message: "map keys are not strings"}
+	}
+
+	for _, k := range keys {
+		if k.String() == "id" {
+			if id, ok := document.MapIndex(k).Interface().(string); ok && id != "" {
+				return id, nil
 			}
+
+			return "", &CosmosError{Code: ErrNoDocumentID, Message: "could not convert id to string"}
+		}
+	}
+
+	return "", &CosmosError{Code: ErrNoDocumentID, Message: "could not find id key in map"}
+}
+
+func extractDocumentIDFromStruct(document reflect.Value) (string, error) {
+	rt := document.Type()
+	numField := rt.NumField()
+
+	for i := 0; i < numField; i++ {
+		if rt.Field(i).Tag.Get("json") == "id" {
+			if id, ok := document.Field(i).Interface().(string); ok && id != "" {
+				return id, nil
+			}
+
+			return "", &CosmosError{Code: ErrNoDocumentID, Message: "could not convert id to string"}
+		}
+	}
+
+	for i := 0; i < numField; i++ {
+		if strings.EqualFold(rt.Field(i).Name, "id") {
+			if id, ok := document.Field(i).Interface().(string); ok && id != "" {
+				return id, nil
+			}
+
+			return "", &CosmosError{Code: ErrNoDocumentID, Message: "could not convert id to string"}
 		}
 	}
 
