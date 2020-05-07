3eb726edfad00a996f390fcccaea81fd8afb37e3
Thomas Lokshall <thomas@lokshall.no>
2020-05-07 09:23:46 +0000

    add document id extractor for maps

--- document.go
+++ document.go
@@ -10,19 +10,38 @@ func DocumentID(document interface{}) (string, error) {
 		rv = rv.Elem()
 	}
 
-	if rv.Kind() != reflect.Struct {
-		return "", &CosmosError{Code: ErrNoDocumentID, Message: "document is not a struct"}
-	}
+	switch rv.Kind() {
+	case reflect.Map:
+		keys := rv.MapKeys()
+		if len(keys) == 0 {
+			return "", &CosmosError{Code: ErrNoDocumentID, Message: "map has no keys"}
+		}
 
-	rt := rv.Type()
-	numField := rt.NumField()
-	for i := 0; i < numField; i++ {
-		if rt.Field(i).Tag.Get("json") == "id" {
-			if id, ok := rv.Field(i).Interface().(string); ok && id != "" {
-				return id, nil
+		if keys[0].Kind() != reflect.String {
+			return "", &CosmosError{Code: ErrNoDocumentID, Message: "map keys are not strings"}
+		}
+
+		for _, k := range keys {
+			if k.String() == "id" {
+				if id, ok := rv.MapIndex(k).Interface().(string); ok && id != "" {
+					return id, nil
+				}
+
+				return "", &CosmosError{Code: ErrNoDocumentID, Message: "could not convert id to string"}
 			}
+		}
 
-			return "", &CosmosError{Code: ErrNoDocumentID, Message: "could not convert id to string"}
+	case reflect.Struct:
+		rt := rv.Type()
+		numField := rt.NumField()
+		for i := 0; i < numField; i++ {
+			if rt.Field(i).Tag.Get("json") == "id" {
+				if id, ok := rv.Field(i).Interface().(string); ok && id != "" {
+					return id, nil
+				}
+
+				return "", &CosmosError{Code: ErrNoDocumentID, Message: "could not convert id to string"}
+			}
 		}
 	}
 
